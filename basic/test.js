0f34c75f2c22cb3c6ca20ac169fa3987bb9696d1
ankit-manhas <159419335+ankit-manhas@users.noreply.github.com>
2025-04-22 17:35:29 +0000

    first

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,35 @@
-console.log(2 > 2);
+
+//    one
+
+
+// function loginuser(username = "sam") {
+// if (!username){
+//     console.log("please enter username")
+//     return
+// }
+
+// return `${username} just loged in`
+
+// }
+
+
+//  console.log(loginuser(""));
+
+// function loginuser(username = "sam") {
+//     if (!username){
+//         console.log("please enter username")
+   
+//     }
+
+//     return `${username} just loged in`
+// }
+
+// console.log(loginuser("ankit"));
+
+
+
+function cart(...num1) {
+  return num1
+}
+
+console.log(cart(200, 300, 400));
